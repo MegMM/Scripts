8ec12112844f2f3c1ceb53eefdb3004bdfaa6447
MegMM <mm_reads@live.com>
2024-07-16 11:36:24 -0700

    Debugger stopped working! Added launch.json. literotica script isn't downloading eveything, maybe?

---
+++ literotica/Headers.py
@@ -0,0 +1,28 @@
+# Response Headers
+# HTTP/2 200 
+# vary: Accept-Encoding
+# etag: W/"5edf63c264de90ed4c314528caf2c2952b0244eb"
+# content-type: text/html; charset=utf-8
+# content-encoding: br
+# server-timing: cache-miss, react-init;dur=2.37;desc="React initialization", react-fetch;dur=259.33;desc="Fetch data from API", react-render;dur=26.4;desc="React app rendering", react-state-serialize;dur=0.63;desc="React state serialization", build-page;dur=58.82;desc="Combine page", total;dur=355.67;desc="Total execution time"
+# date: Mon, 15 Jul 2024 06:23:43 GMT
+# strict-transport-security: max-age=31536000
+# x-frame-options: sameorigin
+# X-Firefox-Spdy: h2
+
+# Request Headers
+# GET /series/se/14186 HTTP/2
+# Host: www.literotica.com
+# User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:128.0) Gecko/20100101 Firefox/128.0
+# Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/png,image/svg+xml,*/*;q=0.8
+# Accept-Language: en-US,en;q=0.5
+# Accept-Encoding: gzip, deflate, br, zstd
+# Connection: keep-alive
+# Cookie: authenticated=1; auth_token=eyJhbGciOiJFUzM4NCIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIwMGUzOGIyNy1mYWNmLTQ4MzYtODI3OS1kMWRlOTEwMWY5OGUiLCJzdWIiOjExNDg3MTMsInByZWZlcnJlZF91c2VybmFtZSI6ImVsaW8iLCJwaWN0dXJlIjoiIiwiZXhwIjoxNzIxMDI2MDY5LCJhY3IiOjAsInVzZXJhZ2VudCI6Ik1vemlsbGEvNS4wIChXaW5kb3dzIE5UIDEwLjA7IFdpbjY0OyB4NjQ7IHJ2OjEyOC4wKSBHZWNrby8yMDEwMDEwMSBGaXJlZm94LzEyOC4wIiwiaXAiOiI3My45My4xMzguNzgiLCJpYXQiOjE3MjEwMjI0Njl9.qlDdHhxxIukdMU5_X6gSV8OGNSC1Au_RMYOXw3LA_0FY-uuUw56rM04YKxxu8MCBVF0Xcsik8pzCqjvi2shz1f-HwOizpIgFRvHpLWGLo962KR5n666ngguyQe6DwGeG; enable_beta_story=1
+# Upgrade-Insecure-Requests: 1
+# Sec-Fetch-Dest: document
+# Sec-Fetch-Mode: navigate
+# Sec-Fetch-Site: cross-site
+# Priority: u=0, i
+# TE: trailers
+# If-None-Match: W/"96a08eedb14b3418661a577e2cc8bbf948c2cbe6"
